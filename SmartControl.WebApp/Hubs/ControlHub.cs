df1b3fe24f7fed31fc3f6d64af482eb38eeb485b
iceStone <ice@micua.com>
2015-08-03 00:19:49 +0800

    init

---
+++ SmartControl.WebApp/Hubs/ControlHub.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+
+namespace SmartControl.Hubs
+{
+    public class ControlHub : Hub
+    {
+        public static IList<ClientInfo> Connections = new List<ClientInfo>();
+
+        public void Login(string token)
+        {
+            Connections.Add(new ClientInfo { Token = token, ConnectionId = Context.ConnectionId });
+        }
+
+        public void ChangeView(string token, string hash)
+        {
+            var tempConnections = Connections.Where(c => c.Token == token);
+            var clients = Clients.Clients(tempConnections.Select(c => c.ConnectionId).ToList());
+            clients.ChangeView(hash);
+        }
+    }
+    public class ClientInfo
+    {
+        public string Token { get; set; }
+        public string ConnectionId { get; set; }
+    }
+}
